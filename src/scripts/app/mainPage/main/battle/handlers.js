98c8345505fca7ddff35d6047cea3e80bbb387fc
Konstantin <beeters88@gmail.com>
2025-08-22 14:58:35 +0300

    feat: add getPickValues funcs

--- src/scripts/app/mainPage/main/battle/handlers.js
+++ src/scripts/app/mainPage/main/battle/handlers.js
@@ -1,4 +1,6 @@
 import { getCurrentEnemy } from "./battleState";
+import getRandomNumber from "../../../../helpers/getRandomNumber";
+
 
 export const handleChangeCheck = () => {
   const attackGroup = document.querySelectorAll('.controls__input_attack');
@@ -16,7 +18,50 @@ export const handleChangeCheck = () => {
   }
 }
 
-export const handleControlsButton = () => {
+const getEnemyValues = (count) => {
+  const parts = ['head', 'neck', 'body', 'arms', 'legs'];
+  const values = [];
+  while (values.length < count) {
+    const index = getRandomNumber(0, parts.length - 1);
+    if (!values.includes(parts[index])) {
+      values.push(parts[index]);
+    }
+  }
+  return values;
+}
+
+const getEnemyPick = () => {
   const enemy = getCurrentEnemy();
-  console.log(enemy)
+  const attackZone = getEnemyValues(enemy.attack);
+  const defenceZone = getEnemyValues(enemy.defence);
+  const hitValue = getRandomNumber(enemy.minHit, enemy.maxHit)
+
+  return {
+    attack: attackZone,
+    defence: defenceZone,
+    hit: hitValue
+  }
+}
+
+const getPlayerPick = () => {
+  const attackGroup = document.querySelectorAll('.controls__input_attack');
+  const defenceGroup = document.querySelectorAll('.controls__input_defence');
+  const attackChecked = [...attackGroup].filter((check) => check.checked).map((check) => check.value);
+  const defenceChecked = [...defenceGroup].filter((check) => check.checked).map((check) => check.value);
+
+  return {
+    attack: attackChecked,
+    defence: defenceChecked,
+    hit: 20
+  }
+}
+
+export const handleControlsButton = () => {
+  const enemyPick = getEnemyPick();
+  const playerPick = getPlayerPick();
+  
+  
+
+  console.log(enemyPick, playerPick)
+
 }
