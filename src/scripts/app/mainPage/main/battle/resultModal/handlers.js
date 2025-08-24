bb6fb1b9df8b4de2b91f377ed852eaf9c6109e52
Konstantin <beeters88@gmail.com>
2025-08-24 19:12:56 +0300

    feat: add render lobby after game

--- src/scripts/app/mainPage/main/battle/resultModal/handlers.js
+++ src/scripts/app/mainPage/main/battle/resultModal/handlers.js
@@ -1,5 +1,8 @@
-
+import lobby from './../../lobby/lobby'
 
 export const handleClickExitButton = (event) => {
   event.target.closest('.result-modal').remove();
+  const main = document.querySelector('.main')
+  main.innerHTML = '';
+  main.append(lobby())
 }
